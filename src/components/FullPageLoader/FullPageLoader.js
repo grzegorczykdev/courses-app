a5372691852b4875b2efff7d597ec6ef40cfb364
Sylwia Grzegorczyk <grzegorczykdev@gmail.com>
2023-09-30 10:56:36 +0200

    spinner component added

--- src/components/FullPageLoader/FullPageLoader.js
+++ src/components/FullPageLoader/FullPageLoader.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Spinner from './Spinner'
 
 import classes from './styles.module.css'
 
@@ -13,7 +14,7 @@ export const FullPageLoader = (props) => {
       className={`${classes.root}${className ? ` ${className}` : ''}`}
       {...otherProps}
     >
-      FullPageLoader
+      <Spinner/>
     </div>
   )
 }
