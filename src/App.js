4636bd821eb6ae9c7ab26a0f3348f577f1c58ac8
Sylwia Grzegorczyk <grzegorczykdev@gmail.com>
2023-09-30 10:44:30 +0200

    application initial state proposed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from 'react'
 
-export const App = () => {
-  return (
-    <div>
-      APP
-    </div>
-  )
+export class App extends React.Component {
+  state = {
+    // global state
+    isLoading: false,
+    hasError: false,
+    errorMessage: '',
+    isInfoDisplayed: false,
+    infoMessage: '',
+
+    // user state
+    isUserLoggedIn: false,
+    userDisplayName: '',
+    userEmail: '',
+    userAvatar: '',
+
+    // router state
+    notLoginUserRoute: 'LOGIN', // 'NEW-ACCOUNT', 'FORGOT PASSWORD'
+
+    // login page state
+    loginEmail: '',
+    loginPassword: '',
+
+    // create account page
+    createAccountEmail: '',
+    createAccountPassword: '',
+    createAccountRepeatPassword: '',
+
+    // recover password page
+    recoverPasswordEmail: '',
+
+    // course list page
+    courses: null,
+    searchPhrase: ''
+  }
+
+  render () {
+    return (
+      <div>
+        APP
+      </div>
+    )
+  }
 }
 
 export default App
