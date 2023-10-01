5617f30d4d0edb3dd1a7a4312c9c72efb3042586
Sylwia Grzegorczyk <grzegorczykdev@gmail.com>
2023-10-01 13:10:53 +0200

    `LoginForm` component filled with content

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Logo from '../Logo'
+import Typography from '../Typography'
+import TextField from '../TextField'
+import Button from '../Button'
+
 import classes from './styles.module.css'
 
 export const LoginForm = (props) => {
@@ -14,7 +19,38 @@ export const LoginForm = (props) => {
       className={`${classes.root}${className ? ` ${className}` : ''}`}
       {...otherProps}
     >
-      Login Form
+      <Logo className={classes.logo}/>
+      <Typography
+        className={classes.header}
+        variant={'h1'}
+      >Log in 👋
+      </Typography>
+      <TextField
+        className={classes.textField}
+        placeholder={'E-mail'}
+      />
+      <TextField
+        className={classes.textField}
+        placeholder={'Password'}
+      />
+      <Button
+        className={classes.button}
+        variant={'contained'}
+        color={'primary'}
+      >LOGIN
+      </Button>
+      <Button
+        className={classes.button}
+        variant={'contained'}
+        color={'secondary'}
+      >CREATE ACCOUNT
+      </Button>
+      <Button
+        className={classes.button}
+        variant={'text'}
+        color={'primary'}
+      >FORGOT PASSWORD
+      </Button>
     </div>
   )
 }
