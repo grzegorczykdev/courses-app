94dcca003f915c9c48d3a48c52f411afcbc5f074
Sylwia Grzegorczyk <grzegorczykdev@gmail.com>
2023-10-14 09:10:19 +0200

    "no courses found" info added

--- src/components/CoursesList/CoursesList.js
+++ src/components/CoursesList/CoursesList.js
@@ -2,7 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import classes from './styles.module.css'
-import CourseCard, {CoursePropType} from '../CourseCard'
+import CourseCard, { CoursePropType } from '../CourseCard'
+import Typography from '../Typography'
 
 export const CoursesList = (props) => {
   const {
@@ -16,28 +17,34 @@ export const CoursesList = (props) => {
       className={`${classes.root}${className ? ` ${className}` : ''}`}
       {...otherProps}
     >
-      {courses && courses.map((course, i) => {
-        const isThirdLeft = i === 0 || i % 3 === 0
-        const isThirdRight = (i + 1) % 3 === 0
-                    return (
-                      <div
-                        className={
+      {
+        courses && courses.length === 0 ?
+          <Typography className={classes.typographyNoCourses}>
+            No courses found
+          </Typography>
+          :
+          courses.map((course, i) => {
+            const isThirdLeft = i === 0 || i % 3 === 0
+            const isThirdRight = (i + 1) % 3 === 0
+            return (
+              <div
+                className={
                           [
                             classes.courseCardWrapper,
                             isThirdLeft ? classes.thirdLeft : null,
-                            isThirdRight ? classes.thirdRight : null,
+                            isThirdRight ? classes.thirdRight : null
                           ]
-                          .filter((className) => className)
-                          .join(' ')
+                            .filter((className) => className)
+                            .join(' ')
                         }
-                        key={course.id}
-                      >
-                        <CourseCard
-                          course={course}
-                        />
-                      </div>
-                    )
-                  })}
+                key={course.id}
+              >
+                <CourseCard
+                  course={course}
+                />
+              </div>
+            )
+          })}
     </div>
   )
 }
