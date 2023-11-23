f55ed916dab37413060bc9e8788efa2aceb0faa8
grzegorczykdev <grzegorczykdev@gmail.com>
2023-11-23 13:45:15 +0100

    `Ratio16x9` component added

--- src/pages/PageCourse/PageCourse.js
+++ src/pages/PageCourse/PageCourse.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
 import { Outlet } from 'react-router-dom'
-
 import { Box } from '@mui/material'
+
+import Ratio16x9 from '../../components/Ratio16x9'
 
 export const PageCourse = (props) => {
   const {
@@ -19,7 +19,17 @@ export const PageCourse = (props) => {
       {...otherProps}
     >
       PageCourse
-      <Outlet/>
+      <Ratio16x9>
+        <Box
+          sx={{ 
+            width: '100%',
+            height: '100%',
+            bgcolor:'#000'
+          }}
+        >
+          <Outlet/>
+        </Box>
+      </Ratio16x9>
     </Box>
   )
 }
